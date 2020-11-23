d06b7677c8dcc363e773b155a1fc0a7ece20dfba
Michal Jirku <box@wejn.org>
2020-11-23 21:40:33 +0100

    Customize tale theme (& add sidenotes+footnotes).
    This is a large-ish commit that adds bunch of features to the base
    "tale" theme forked from https://github.com/chesterhow/tale at de5cd20.
    
    The changes:
    
    - custom footer
    - support for sidenotes, original taken from:
      https://blog.jkl.gg/jekyll-footnote-tufte-sidenote/
    - logo at the top of the website
    - date format is %Y-%m-%d
    - The "written by" in article is only shown when article's author
      differs from site author
    - Pages subpage shows list of posts sorted by years

---
+++ _pages/posts.md
@@ -0,0 +1,50 @@
+---
+layout: default
+title: "Posts"
+permalink: /posts/
+---
+
+<div class="tags">
+  <div class="tags-header">
+    <h2 class="tags-header-title">{{ page.title }}</h2>
+    <div class="tags-header-line"></div>
+  </div>
+  <div class="tags-clouds">
+    {% for post in site.posts %}
+    {% capture this_year %}{{ post.date | date: "%Y" }}{% endcapture %}
+    {% capture next_year %}{{ post.previous.date | date: "%Y" }}{% endcapture %}
+    {% if forloop.first %}<a href="#{{ this_year }}">{{ this_year }}</a>{% endif %}
+    {% unless forloop.last %}
+    {% if this_year != next_year %}<a href="#{{ next_year }}">{{ next_year }}</a>{% endif %}
+    {% endunless %}
+    {% endfor %}
+  </div>
+  {% for post in site.posts %}
+    {% capture this_year %}{{ post.date | date: "%Y" }}{% endcapture %}
+    {% capture next_year %}{{ post.previous.date | date: "%Y" }}{% endcapture %}
+    {% if forloop.first %}
+  <div class="tags-item" id="{{ this_year }}">
+    <h2 class="tags-item-label">{{ this_year }}</h2>
+    {% endif %}
+    <a class="tags-post" href="{{ post.url | prepend: site.baseurl }}">
+      <div>
+        <span class="tags-post-title">{{ post.title }}</span>
+        <div class="tags-post-line"></div>
+      </div>
+      <span class="tags-post-meta">
+        <time datetime="{{ post.date }}">
+          {{ post.date | date:"%Y-%m-%d" }}
+        </time>
+      </span>
+    </a>
+    {% if forloop.last %}
+  </div>
+    {% else %}
+        {% if this_year != next_year %}
+  </div>
+  <div class="tags-item" id="{{ next_year }}">
+    <h2 class="tags-item-label">{{ next_year }}</h2>
+        {% endif %}
+    {% endif %}
+  {% endfor %}
+</div>
---
+++ assets/js/sidenotes.js
@@ -0,0 +1,103 @@
+/*
+ * @author: Kaushik Gopal
+ * @author: Michal Jirků
+ *
+ * A jQuery function that displays the footnotes
+ * on the side (sidenotes) for easier reading.
+ *
+ * This is as recommended by Edward Tufte's style sidenotes:
+ * https://edwardtufte.github.io/tufte-css/#sidenotes
+ *
+ * ---
+ *
+ *  Changelog:
+ *  - 2020-11-23, Michal Jirků
+ *    - make sidenotes flexible width
+ *    - make sidenotes not overlap one another
+ *    - make sidenotes reload on font load (to help with &previous)
+ *    - make sidenotes compatible with multiple links to the same note
+ *  - 2020-09-07, Kaushik Gopal
+ *    - initial version from https://blog.jkl.gg/jekyll-footnote-tufte-sidenote/
+ *
+ */
+(function () {
+
+    function showSidenote(index, sup, ww, $fnli, minh) {
+
+        // construct sidenote div
+        let content = $fnli.eq(index).html();
+        let div = $(document.createElement('div'));
+        div.html("<span class=\"sidenote-header\">" + (index+1) + ". </span>" +
+            content);
+        div.addClass("sidenote");
+        let position = sup.offset();
+
+        div.css({
+            position: "absolute",
+            left: ww,
+            top: minh > position["top"] ? minh : position["top"],
+            'min-width': ww > 420 ? ww/4 : ww/3,
+            'max-width': ww/3,
+        });
+
+        if (ww > 420) {
+            sup.hover(function() {
+                div.addClass("sidenote-hover");
+            }, function () {
+                div.removeClass("sidenote-hover");
+            });
+        } else {
+            div.addClass("sidenote-hover");
+        }
+
+        // console.log("props " + ww + " fn: " + $fnli.text());
+
+        $(document.body).append(div);
+
+        // console.log("sidenote: " + index + " top: " + div.position().top + " height: " + div.height());
+
+        return div.position().top + div.height() + 5;
+    }
+
+    function loadSidenotes(ww, $fnli, fncount, $fn) {
+        let minh = 0;
+        $("sup[role='doc-noteref']").each(function(index, el) {
+            minh = showSidenote(el.textContent - 1, $(this), ww, $fnli, minh);
+        });
+        if (ww > 420) {
+            $fn.hide();
+        } else {
+            $fn.show();
+        }
+
+    }
+
+    $(window).on("load", function() {
+        let $fn = $(".footnotes"),
+            $fnli = $fn.find("ol li"),
+            fncount = $fnli.length,
+            ww = $("main").outerWidth() + $("main").offset().left;
+
+        // load first time
+        loadSidenotes(ww, $fnli, fncount, $fn);
+
+        $(window).resize(function() {
+            const new_ww = $("main").outerWidth() + $("main").offset().left;
+            if (new_ww === ww) return;
+
+            // console.log(" XXX -- RESIZE -- XXX ");
+            ww = new_ww;
+            $(".sidenote").remove();
+            loadSidenotes(ww, $fnli, fncount, $fn);
+        });
+
+        // fonts have a power to blow up the size of the sidenotes, hence the
+        // need to adjust
+        document.fonts.onloadingdone = function(fss) {
+            ww = $("main").outerWidth() + $("main").offset().left;
+            $(".sidenote").remove();
+            loadSidenotes(ww, $fnli, fncount, $fn);
+        };
+    });
+
+})();
